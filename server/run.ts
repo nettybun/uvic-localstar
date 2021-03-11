24f635fab53e23a9e482842fe5ac5ac1839eebf1
Gen Hames <hamesg@tutanota.de>
2021-03-11 12:30:09 -0800

    General run script

--- server/run.ts
+++ server/run.ts
@@ -6,23 +6,32 @@ const isWindows = Deno.build.os === "windows";
 const osPath = (path: string) => isWindows ? path.replaceAll("/", SEP) : path;
 
 const sh = async (cmd: string, ...other: string[]) => {
-  const cmdArr = [...cmd.split(" "), ...other].map(osPath);
+  const cmdArr = [
+    ...cmd
+      .replaceAll(/(\n|\s)+/g, " ")
+      .split(" "),
+    ...other,
+  ].map(osPath);
   console.log("$", cmdArr.join(" "));
   const p = Deno.run({ cmd: cmdArr });
   await p.status();
 };
 
+const outdir = "./bin";
+
+Deno.removeSync(outdir, { recursive: true });
+
 await sh(
-  `deno run -A --unstable ./scripts/compile.ts --lite --output=bin/[name]-[target]-${Deno.version.deno} ./localstar.ts`,
+  `deno run -A --unstable ./scripts/compile.ts --lite
+    --output=${outdir}/[name]-[target]-${Deno.version.deno} ./localstar.ts`,
 );
 const bins = [];
-for await (const file of fs.walk(osPath("./bin/"), { includeDirs: false })) {
+for await (const file of fs.walk(osPath(outdir), { includeDirs: false })) {
   bins.push(file.path);
 }
 await sh(
   `deno run -A --unstable ./scripts/embed.ts`,
-  // TODO: I think the binary can have spaces in it?...
   ...bins,
   `--root=../client/build/`,
-  `--root=../starboard/`,
+  // `--root=../starboard/`,
 );
